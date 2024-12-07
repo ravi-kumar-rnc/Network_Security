bbbc96f250937e5bccf594ca96506182b3700179
ravi-kumar-rnc <ravi.kumar.rnc@gmail.com>
2024-12-07 06:20:21 +0530

    Training Pipeline Created

---
+++ networksecurity/pipeline/training_pipeline.py
@@ -0,0 +1,43 @@
+import os
+import sys
+
+from networksecurity.components.data_ingestion import DataIngestion
+from networksecurity.components.data_transformation import DataTransformation
+from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.model_evaluation import ModelEvaluation
+from networksecurity.components.model_trainer import ModelTrainer
+from networksecurity.components.model_pusher import ModelPusher
+
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logger.logger import logging
+
+from networksecurity.entity.config_entity import(
+    TrainingPipelineConfig,
+    DataIngestionConfig,
+    DataTransformationConfig,
+    DataValidationConfig,
+    ModelEvaluationConfig,
+    ModelTrainerConfig,
+    ModelPusherConfig
+)
+
+from networksecurity.entity.artifact_entity import(
+    TrainingPipelineArtifact,
+    DataIngestionArtifact,
+    DataTransformationArtifact,
+    DataValidationArtifact,
+    ModelEvaluationArtifact,
+    ModelTrainerArtifact,
+    ModelPusherArtifact
+)
+
+
+class TrainingPipeline:
+    def __init__(self):
+        pass
+
+    def start_data_ingestion(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
