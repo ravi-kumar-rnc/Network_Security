d645026e469d974ae1c162318f321d7e04df944f
ravi-kumar-rnc <ravi.kumar.rnc@gmail.com>
2024-12-08 06:56:05 +0530

    Config&Artifact_Structure

---
+++ networksecurity/entity/artifact_entity.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionArtifact:
+    pass
+
+@dataclass
+class DataTransformationArtifact:
+    pass
+@dataclass
+class DataValidationArtifact:
+    pass
+@dataclass
+class ModelTrainerArtifact:
+    pass
+
+@dataclass
+class ModelEvaluationArtifact:
+    pass
+
+@dataclass
+class ModelPusherArtifact:
+    pass
+
+@dataclass
+class ClassificationMetricArtifact:
+    pass
