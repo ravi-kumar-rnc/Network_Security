3df94326efec3ef2b3b64996908ba49758eaf676
ravi-kumar-rnc <ravi.kumar.rnc@gmail.com>
2024-12-10 22:21:01 +0530

    Data_Ingestion code

---
+++ networksecurity/constant/training_pipeline/__init__.py
@@ -0,0 +1,68 @@
+import os
+import sys
+import pandas as pd
+import numpy as np
+
+
+
+"""
+defining common constant variable for training pipeline
+"""
+
+PIPELINE_NAME:str = "NetworkSecurity"
+ARTIFACT_DIR:str = "Artifacts"
+FILE_NAME:str = "NetworkData.csv"
+TARGET_COLUMN:str = "Result"
+
+TRAIN_FILE_NAME:str = "train.csv"
+TEST_FILE_NAME:str = "test.csv"
+
+PREPROCESSING_OBJECT_FILE_NAME = "preprocessing.pkl"
+MODEL_FILR_NAME = "model.pkl"
+SCHEMA_FILE_PATH = os.path.join("data_schema", "schema.yaml")
+SCHEMA_DROP_COLS = "drop_columns"
+SAVED_MODEL_DIR = "saved_models"
+
+
+
+"""
+Data Ingestion related constant start with DATA_INGESTION var name
+"""
+
+DATA_INGESTION_COLLECTION_NAME: str = "NetworkData"
+DATA_INGESTION_DATABASE_NAME: str = "NetworkDataTest"
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+DATA_INGESTION_INGESTED_DIR: str = "ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATION: float = 0.2
+
+
+"""
+Data Validation related constant start with DATA_VALIDATION var name
+"""
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+DATA_VALIDATION_VALID_DIR: str = "validated"
+DATA_VALIDATION_INVALID_DIR: str = "invalid"
+DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
+DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
+
+"""
+Data Transformation related constant start with DATA_TRANSFORMATION var name
+"""
+
+
+
+
+"""
+Model Trainer related constant start with MODEL_TRAINER var name
+"""
+
+"""
+Model  Evaluation constant start with MODEL_EVALUATION var name
+"""
+
+"""
+Model Pusher related constant start with MODEL_PUSHER var name
+"""
+
