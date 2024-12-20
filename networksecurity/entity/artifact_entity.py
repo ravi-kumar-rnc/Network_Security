79cceda5d9d1594b229fca2a7a3e88959a41a2b8
ravi-kumar-rnc <ravi.kumar.rnc@gmail.com>
2024-12-20 22:21:36 +0530

    Data Validation

--- networksecurity/entity/artifact_entity.py
+++ networksecurity/entity/artifact_entity.py
@@ -10,7 +10,12 @@ class DataTransformationArtifact:
     pass
 @dataclass
 class DataValidationArtifact:
-    pass
+    validation_status: bool
+    valid_train_file_path: str
+    valid_test_file_path: str
+    invalid_train_file_path: str
+    invalid_test_file_path: str
+    drift_report_file_path: str
 @dataclass
 class ModelTrainerArtifact:
     pass
